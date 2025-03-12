5f6fd6bd5d392277d90491bb292a6903a86f42a2
Armughan-Ather <armughan.ather83@gmail.com>
2025-03-12 13:34:52 +0500

    created user,messages models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):  
+    email = models.EmailField(unique=True)  
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    groups = models.ManyToManyField(
+        "auth.Group", related_name="api_users"
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission", related_name="api_users_permissions"
+    )
+
+    def __str__(self):
+        return self.username
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_messages")
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender} -> {self.receiver}: {self.content[:30]}"
